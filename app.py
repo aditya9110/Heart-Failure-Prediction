ca3a7aad86aadac7d129f1dce82999a43601a14a
Aditya Mahimkar <mahimkaradi@gmail.com>
2021-11-03 11:54:26 +0530

    app updated

--- app.py
+++ app.py
@@ -16,7 +16,12 @@ from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, cross_val_
 from sklearn.svm import SVC
 from imblearn.over_sampling import RandomOverSampler, SMOTE
 
-st.title('Heart Failure Prediction')
+row0col1, row0col2 = st.columns([1, 5])
+with row0col1:
+    st.image('1249-heart-beat-outline.gif')
+with row0col2:
+    st.title('Heart Failure Prediction')
+
 data = pd.read_csv('heart_failure_clinical_records_dataset.csv')
 
 labelled_data = data.copy()
@@ -47,7 +52,22 @@ if page == 'Home':
 
     st.write('[Data Source - Kaggle](https://www.kaggle.com/andrewmvd/heart-failure-clinical-data)')
 
-if page == 'Visualize':
+elif page == 'Visualize':
+    st.header('Independent Variables')
+    row0a, row0b, row0c = [], [], []
+    row0a.extend(st.columns([1, 1, 1.5, 1]))
+    row0b.extend(st.columns(4))
+    row0c.extend(st.columns(4))
+    for i in range(4):
+        row0a[i].write(data.columns[i])
+        row0b[i].write(data.columns[i + 4])
+        row0c[i].write(data.columns[i + 8])
+
+    st.header('Dependent Variable')
+    st.subheader('DEATH_EVENT')
+    st.write('The label here considered is DEATH_EVENT which depicts the death of a patient during the follow-up period'
+             ' of the treatment.')
+
     # discrete vs continuous features
     discrete_features, continuous_features = [], []
     for feature in data.columns:
@@ -69,6 +89,19 @@ if page == 'Visualize':
         for feature in continuous_features:
             st.write(feature)
 
+    feature_details = {'anaemia': 'Anaemia is a condition in which the blood doesn\'t have enough healthy red blood cells.',
+                       'diabetes': 'Having diabetes means you are more likely to develop heart disease.',
+                       'high_blood_pressure': 'High blood pressure is a condition in which the force of the blood against the artery walls is too high. High blood pressure can also cause ischemic heart disease.',
+                       'sex': 'Sex of the patient.',
+                       'smoking': 'It describe the smoking habit of the patient.',
+                       'age': 'Age of the patient. The average age of patient is ' + str(round(data['age'].mean(), 2)),
+                       'creatinine_phosphokinase': 'Determines the level of CPK enzyme in the blood. Higher the level, more chance of heart disease.',
+                       'ejection_fraction': 'Percentage of fluid ejected from heart chamber with each contraction. A borderline ejection fraction can range between 41% and 50%.',
+                       'platelets': 'A normal platelet count ranges from 150,000 to 450,000 platelets per microliter of blood.',
+                       'serum_creatinine': 'The measure of serum creatinine may also be used to estimate how quickly the kidneys filter blood. 0.5 to 1.2 (mg/dL) is a normal range. Higher than that results in kidney impairment. ',
+                       'serum_sodium': 'Indicates the level of serum sodium in the blood. A normal blood sodium level is between 135 and 145 milliequivalents per liter (mEq/L). If your sodium blood levels are too high or too low, it may mean that you have a problem with your kidneys, dehydration, or another medical condition.',
+                       'time': 'Determines the follow-up period of each patient. Most patients who deceased were in the initial follow-up period.'}
+
     # count plot
     st.header('Counts and Count Difference')
     row2col1, row2col2 = st.columns([1, 2.5])
@@ -77,7 +110,7 @@ if page == 'Visualize':
         option1 = st.radio('', discrete_features + label)
     with row2col2:
         if option1 == 'DEATH_EVENT':
-            st.subheader(option1)
+            st.subheader(option1 + ' - Dependent Variable')
             fig = plt.figure(figsize=(6, 4))
             plt.xticks(fontsize=12)
             sns.countplot(x=option1, data=labelled_data)
@@ -85,20 +118,22 @@ if page == 'Visualize':
             st.subheader(option1)
             fig, ax = plt.subplots(1, 2, figsize=(6, 4))
             plt.xticks(fontsize=12)
-            sns.countplot(ax=ax[0], x=option1, data=labelled_data)
-            sns.countplot(ax=ax[1], x=option1, hue='DEATH_EVENT', data=labelled_data)
+            sns.countplot(ax=ax[0], x=option1, data=labelled_data).set_title('A) Count Difference')
+            sns.countplot(ax=ax[1], x=option1, hue='DEATH_EVENT', data=labelled_data).set_title('B) Count wrt DEATH_EVENT')
         st.pyplot(fig)
+
+    st.write(feature_details[option1])
 
     # correlation matrix
     st.header('Correlation Matrix')
+    selected_features = st.multiselect('Features', data.columns)
     row3col1, row3col2 = st.columns([6, 1])
     correlate = 0
     with row3col1:
-        selected_features = st.multiselect('Features', data.columns)
+        select_all_features = st.checkbox('Select All Features')
     with row3col2:
         if st.button('Correlate'):
             correlate = 1
-    select_all_features = st.checkbox('Select All Features')
     if select_all_features:
         selected_features = list(data.columns)
     if selected_features and (select_all_features or correlate):
@@ -121,6 +156,8 @@ if page == 'Visualize':
         sns.kdeplot(x=option2, hue='DEATH_EVENT', data=data, fill=True)
         st.pyplot(fig)
 
+    st.write(feature_details[option2])
+
     st.header('Feature Selection')
     best_features = SelectKBest(chi2, k=10)
     features_ranking = best_features.fit(data.drop(['DEATH_EVENT'], axis=1), data['DEATH_EVENT'])
@@ -128,10 +165,30 @@ if page == 'Visualize':
     ranking_dictionary = {}
     for i in range(len(features_ranking.scores_)):
         ranking_dictionary[data.columns[i]] = round(features_ranking.scores_[i], 3)
-    print(ranking_dictionary)
-    st.bar_chart(pd.Series(ranking_dictionary))
+    asc_sort = sorted(ranking_dictionary.items(), key=lambda kv: (kv[1], kv[0]))
+    plot_data = {'features': [i for i, j in asc_sort], 'scores': list(np.arange(1, 13))}
+    print(plot_data)
 
-if page == 'Predict':
+    # st.bar_chart(pd.Series(plot_data))
+
+
+    def predict(features, algo, sample):
+        pass
+
+
+    st.header('Model Building')
+    model_input_features = st.multiselect('Features', data.drop(['DEATH_EVENT'], axis=1).columns)
+    algos = ['Logistic Regression', 'Random Forest Classifier', 'Decision Tree Classifier', 'Gradient Boosting', 'SVM']
+    row5col1, row5col2 = st.columns(2)
+    with row5col1:
+        sample_method = st.radio('Select Sample Method', ['No Sampling', 'Over Sampling', 'SMOTE'])
+    with row5col2:
+        ml_algorithm = st.selectbox('Select Algorithm', algos)
+    if st.button('Predict'):
+        if model_input_features:
+            predict(model_input_features, ml_algorithm, sample_method)
+
+elif page == 'Predict':
     with st.form(key='my_form'):
         name_input = st.text_input(label='Enter Name')
         age = st.slider('Age', min_value=30, max_value=100)
